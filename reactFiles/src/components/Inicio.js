dd64d3e77b1253f2927fd82a613e1aed8e09bc74
Wesley Souza <wesleyadriann@gmail.com>
2019-03-22 10:48:26 -0300

    Move os arquivos.

--- reactFiles/src/components/Inicio.js
+++ reactFiles/src/components/Inicio.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Inicio = () => {
     return (
         <div>
-            <p className="h1">Wesley Souza</p>
+            <p className="h1">Wesley Adriann</p>
             <p className="h4">Bacharelando em Ciência da Computação</p>
         </div>
     )
